811067fad51c20ab9d10e632a04c83bf210572bc
crokant <zavoronkovviktor87@gmail.com>
2024-04-22 07:38:40 +0300

    laba1
    Signed-off-by: crokant <zavoronkovviktor87@gmail.com>

--- Laba1/LABA1_Mephi_Remake/test.c
+++ Laba1/LABA1_Mephi_Remake/test.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "fieldinfo.h"
 #include "vector.h"
 #include "double.h"
@@ -7,7 +6,7 @@
 #include "integer.h"
 #include "test.h"
 
-void runIntegerTest(const char* filename) {
+void runTest(const char* filename, FieldInfo* fieldInfo) {
     FILE *file = fopen(filename, "r");
 
     if (!file) {
@@ -15,20 +14,19 @@ void runIntegerTest(const char* filename) {
         return;
     }
 
-    FieldInfo *intInfo = getIntImplementation();
     int size;
     fscanf(file, "%d", &size);
 
-    Vector *vector1 = newVector(size, intInfo);
-    Vector *vector2 = newVector(size, intInfo);
-    Vector *result = newVector(size, intInfo);
+    Vector *vector1 = newVector(size, fieldInfo);
+    Vector *vector2 = newVector(size, fieldInfo);
+    Vector *result = newVector(size, fieldInfo);
 
     for (int i = 0; i < size; ++i) {
-        intInfo->inputFromFile(file, vector1->data + i * intInfo->elemSize);
+        vector1->base->inputFromFile(file, vector1->data + i * vector1->base->elemSize);
     }
 
     for (int i = 0; i < size; ++i) {
-        intInfo->inputFromFile(file, vector2->data + i * intInfo->elemSize);
+        vector2->base->inputFromFile(file, vector2->data + i * vector2->base->elemSize);
     }
 
     printf("Vector 1: ");
@@ -49,4 +47,14 @@ void runIntegerTest(const char* filename) {
     fclose(file);
 }
 
+void runIntegerTest(const char* filename) {
+    runTest(filename, getIntImplementation());
+}
 
+void runDoubleTest(const char* filename) {
+    runTest(filename, getDoubleImplementation());
+}
+
+void runComplexTest(const char* filename) {
+    runTest(filename, getComplexImplementation());
+}
