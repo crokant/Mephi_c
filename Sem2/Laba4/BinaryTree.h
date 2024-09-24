363643d10f67d05e6dd0e02ae4bc05b4401cd6b6
crokant <zavoronkovviktor87@gmail.com>
2024-09-24 14:00:36 +0300

    Merge remote-tracking branch 'origin/final' into final

--- Sem2/Laba4/BinaryTree.h
+++ Sem2/Laba4/BinaryTree.h
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <sstream>
 #include <functional>
 #include <stdexcept>
---
+++ Sem3/Laba1/SmartPtr.h
@@ -0,0 +1,11 @@
+#pragma once
+
+template<class T>
+class SmartPtr {
+private:
+    T *data;
+    int counter;
+
+public:
+
+};
